85bca4ab0646c41e82ea5d220ebc8565c0728da1
Iisyourdad <76657591+Iisyourdad@users.noreply.github.com>
2025-03-30 20:23:50 -0500

    updated css

--- run_kiosk.sh
+++ run_kiosk.sh
@@ -1,3 +1,3 @@
 sleep 5
 
-/bin/chromium-browser  --kiosk  --ozone-platform=wayland --start-maximized http:127.0.0.1:8000
+/bin/chromium-browser  --touch-events=enabled --enable-pinch --enable-touch-drag-drop --kiosk  --ozone-platform=wayland --start-maximized http:127.0.0.1:8000
